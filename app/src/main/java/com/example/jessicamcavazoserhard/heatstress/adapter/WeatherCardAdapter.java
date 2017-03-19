e4e717850335dacb3c1dbcd11b6ab01019cab174
LuisHernandez96 <luis_fernando_hernandez@outlook.com>
2017-03-19 17:34:28 -0600

    WeatherCard

--- app/src/main/java/com/example/jessicamcavazoserhard/heatstress/adapter/WeatherCardAdapter.java
+++ app/src/main/java/com/example/jessicamcavazoserhard/heatstress/adapter/WeatherCardAdapter.java
@@ -1,8 +1,94 @@
 package com.example.jessicamcavazoserhard.heatstress.adapter;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.jessicamcavazoserhard.heatstress.R;
+import com.example.jessicamcavazoserhard.heatstress.model.WeatherCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Sabba on 3/19/2017.
  */
 
-public class WeatherCardAdapter {
+public class WeatherCardAdapter extends RecyclerView.Adapter<WeatherCardAdapter.WeatherCardHolder>{
+
+    private List<WeatherCard> listData;
+    private LayoutInflater inflater;
+
+    public WeatherCardAdapter(List<WeatherCard> listData, Context c){
+        inflater = LayoutInflater.from(c);
+        this.listData = listData;
+    }
+
+    @Override
+    public WeatherCardAdapter.WeatherCardHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.hourly_wether_card, parent, false);
+        return new WeatherCardHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(WeatherCardHolder holder, int position) {
+        WeatherCard item = listData.get(position);
+        holder.hour.setText(item.getsHour());
+        holder.temp.setText(String.valueOf(item.getiTemperature())+" °F");
+        holder.hum.setText(String.valueOf(item.getiHumidity())+" %");
+        switch (item.getsWeather()){
+            case "Rainy": holder.weather_icon.setImageResource(R.drawable.rain);
+                break;
+            case "Sunny": holder.weather_icon.setImageResource(R.drawable.sun);
+                break;
+            case "PartCloudy": holder.weather_icon.setImageResource(R.drawable.sun_cloud);
+                break;
+            case "Cloudy": holder.weather_icon.setImageResource(R.drawable.cloud);
+                break;
+            case "Windy": holder.weather_icon.setImageResource(R.drawable.wind);
+                break;
+            case "Snowy": holder.weather_icon.setImageResource(R.drawable.snow);
+                break;
+        }
+
+        holder.temp_icon.setImageResource(R.drawable.temp);
+        holder.hum_icon.setImageResource(R.drawable.drops);
+    }
+
+    public void setListData(ArrayList<WeatherCard> exerciseList) {
+        this.listData.clear();
+        this.listData.addAll(exerciseList);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    class WeatherCardHolder extends RecyclerView.ViewHolder{
+
+        ImageView weather_icon, temp_icon, hum_icon;
+        TextView hour, temp, hum;
+        View container;
+
+        public WeatherCardHolder(View itemView) {
+            super(itemView);
+            weather_icon = (ImageView)itemView.findViewById(R.id.iv_weather_card);
+            temp_icon = (ImageView)itemView.findViewById(R.id.iv_temp_card);
+            hum_icon = (ImageView)itemView.findViewById(R.id.iv_hum_card);
+
+            hour = (TextView)itemView.findViewById(R.id.tv_hour_card);
+            temp = (TextView) itemView.findViewById(R.id.tv_temp_card);
+            hum = (TextView) itemView.findViewById(R.id.tv_hum_card);
+
+            container = (View)itemView.findViewById(R.id.cont_item_root);
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/jessicamcavazoserhard/heatstress/ui/MainActivity.java
+++ app/src/main/java/com/example/jessicamcavazoserhard/heatstress/ui/MainActivity.java
@@ -3,19 +3,40 @@ package com.example.jessicamcavazoserhard.heatstress.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.jessicamcavazoserhard.heatstress.R;
+import com.example.jessicamcavazoserhard.heatstress.adapter.WeatherCardAdapter;
+import com.example.jessicamcavazoserhard.heatstress.model.WeatherCard;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button btGo;
+    private RecyclerView recyclerView;
+    private WeatherCardAdapter adapter;
+    private ArrayList<WeatherCard> listData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        listData = getData();
+
+        recyclerView = (RecyclerView) findViewById(R.id.rec_view_main);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new WeatherCardAdapter(listData,this);
+        recyclerView.setAdapter(adapter);
 
         btGo = (Button) findViewById(R.id.button_go);
         btGo.setOnClickListener(this);
@@ -25,5 +46,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         Intent i = new Intent(this , MoreInfoActivity.class);
         startActivity(i);
+    }
+
+    ArrayList<WeatherCard> getData(){
+        ArrayList<WeatherCard> dummyData = new ArrayList<>();
+        WeatherCard dummy = new WeatherCard("11:00 AM","Sunny",80,20);
+        dummyData.add(dummy);
+        dummy = new WeatherCard("12:00 PM","Sunny",80,20);
+        dummyData.add(dummy);
+        dummy = new WeatherCard("1:00 PM","Sunny",80,20);
+        dummyData.add(dummy);
+        dummy = new WeatherCard("2:00 PM","PartCloudy",70,30);
+        dummyData.add(dummy);
+        dummy = new WeatherCard("3:00 PM","PartCloudy",70,30);
+        dummyData.add(dummy);
+        dummy = new WeatherCard("4:00 PM","PartCloudy",70,50);
+        dummyData.add(dummy);
+        dummy = new WeatherCard("5:00 PM","Cloudy",60,60);
+        dummyData.add(dummy);
+        dummy = new WeatherCard("6:00 PM","Cloudy",60,60);
+        dummyData.add(dummy);
+        dummy = new WeatherCard("7:00 PM","Rainy",50,100);
+        dummyData.add(dummy);
+        dummy = new WeatherCard("8:00 PM","Rainy",50,100);
+        dummyData.add(dummy);
+        dummy = new WeatherCard("9:00 PM","Windy",50,90);
+        dummyData.add(dummy);
+        dummy = new WeatherCard("10:00 PM","Snowy",0,10);
+        dummyData.add(dummy);
+
+        return dummyData ;
     }
 }
