3f2b36b73f4bda5e0749bdb08c0a29b8a1de950a
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2017-03-29 22:58:01 -0600

    API Implementation on MainActivity (JUST WORKS FOR CALIFORNIA CITIES), still missing JSON parsing into actual data

--- app/src/main/java/com/example/jessicamcavazoserhard/heatstress/ui/MainActivity.java
+++ app/src/main/java/com/example/jessicamcavazoserhard/heatstress/ui/MainActivity.java
@@ -1,26 +1,36 @@
 package com.example.jessicamcavazoserhard.heatstress.ui;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.jessicamcavazoserhard.heatstress.R;
 import com.example.jessicamcavazoserhard.heatstress.adapter.WeatherCardAdapter;
 import com.example.jessicamcavazoserhard.heatstress.model.WeatherCard;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     ImageButton btGo;
+    EditText etLocation;
     private RecyclerView recyclerView;
     private WeatherCardAdapter adapter;
     private ArrayList<WeatherCard> listData;
+
+    String Location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         recyclerView.setAdapter(adapter);
 
         btGo = (ImageButton) findViewById(R.id.imageButton_risk);
+        etLocation = (EditText) findViewById(R.id.editText_address);
         btGo.setOnClickListener(this);
     }
 
@@ -78,4 +89,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return dummyData ;
     }
+
+    // IMPLEMENTATION ASYNCTASK FOR WEATHER API REQUEST
+    // KEY - 25d0f02c485109f2
+
+    class RetrieveWeatherForLocation extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+
+            Location = etLocation.getText().toString();
+        }
+
+        protected String doInBackground(Void... urls) {
+
+            // Do some validation here
+            Location = Location.replaceAll("\\s+", "_");
+            Log.d("Conecion","String trnsformed: " + Location);
+
+            try {
+                URL url = new URL("http://api.wunderground.com/api/25d0f02c485109f2/conditions/hourly/q/CA/"+Location+".json");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                Log.d("Conecion","Retrieving weather from: " + url);
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            Log.i("INFO", response);
+        }
+    }
+
+
+
 }
